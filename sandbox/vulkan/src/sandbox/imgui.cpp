ca15150b5728462693a1f79df5e72001ad35920b
Davide Tuccilli <d.tuccilli@gmail.com>
2022-06-15 00:59:50 +0200

    Hacky solution for transparency issue with imgui without render passes

--- sandbox/vulkan/src/sandbox/imgui.cpp
+++ sandbox/vulkan/src/sandbox/imgui.cpp
@@ -10,6 +10,8 @@
 #include <SDL.h>
 #include <imgui.h>
 
+#include <cstring>
+
 namespace oblo::vk
 {
     bool imgui::init(SDL_Window* window,
@@ -51,6 +53,7 @@ namespace oblo::vk
         }
 
         m_context = ImGui::CreateContext();
+        ImGui::GetIO().IniFilename = "vksandbox_imgui.ini";
 
         ImGui_ImplSDL2_InitForVulkan(window);
 
@@ -63,7 +66,7 @@ namespace oblo::vk
                                            .ImageCount = swapchainImageCount,
                                            .MSAASamples = VK_SAMPLE_COUNT_1_BIT};
 
-        if (ImGui_ImplVulkan_Init(&initInfo, nullptr))
+        if (ImGui_ImplVulkan_Init(&initInfo, nullptr) && create_dummy_pipeline(device))
         {
             const VkCommandBufferBeginInfo beginInfo = {.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
                                                         .flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT};
@@ -102,6 +105,8 @@ namespace oblo::vk
             vkDestroyDescriptorPool(device, m_descriptorPool, nullptr);
             m_descriptorPool = nullptr;
         }
+
+        destroy_dummy_pipeline(device);
     }
 
     void imgui::process(const SDL_Event& event)
@@ -117,6 +122,39 @@ namespace oblo::vk
         ImGui_ImplSDL2_NewFrame();
 
         ImGui::NewFrame();
+    }
+
+    namespace
+    {
+        void read_imgui_vulkan_state(VkPipelineLayout* pipelineLayout,
+                                     VkShaderModule* vertexShaderModule,
+                                     VkShaderModule* fragmentShaderModule)
+        {
+            static_assert(IMGUI_VERSION_NUM == 18700, "This struct should be checked in case of version update");
+
+            struct ImGui_ImplVulkan_Data_Header
+            {
+                ImGui_ImplVulkan_InitInfo VulkanInitInfo;
+                VkRenderPass RenderPass;
+                VkDeviceSize BufferMemoryAlignment;
+                VkPipelineCreateFlags PipelineCreateFlags;
+                VkDescriptorSetLayout DescriptorSetLayout;
+                VkPipelineLayout PipelineLayout;
+                VkPipeline Pipeline;
+                uint32_t Subpass;
+                VkShaderModule ShaderModuleVert;
+                VkShaderModule ShaderModuleFrag;
+            };
+
+            ImGui_ImplVulkan_Data_Header header;
+
+            const auto* userdata = ImGui::GetIO().BackendRendererUserData;
+            std::memcpy(&header, userdata, sizeof(header));
+
+            *pipelineLayout = header.PipelineLayout;
+            *vertexShaderModule = header.ShaderModuleVert;
+            *fragmentShaderModule = header.ShaderModuleFrag;
+        }
     }
 
     void imgui::end_frame(VkCommandBuffer commandBuffer, VkImageView imageView, u32 width, u32 height)
@@ -143,8 +181,133 @@ namespace oblo::vk
             .pColorAttachments = &colorAttachmentInfo,
         };
 
+        // For some reason, alpha blending doesn't seem to work with imgui when using the dynamic rendering
+        // extension
+        vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_dummyPipeline);
+
         vkCmdBeginRendering(commandBuffer, &renderInfo);
         ImGui_ImplVulkan_RenderDrawData(drawData, commandBuffer);
         vkCmdEndRendering(commandBuffer);
+    }
+
+    bool imgui::create_dummy_pipeline(VkDevice device)
+    {
+        VkPipelineLayout pipelineLayout;
+        VkShaderModule vertexShaderModule, fragmentShaderModule;
+
+        read_imgui_vulkan_state(&pipelineLayout, &vertexShaderModule, &fragmentShaderModule);
+
+        const VkPipelineShaderStageCreateInfo vertShaderStageInfo{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+            .stage = VK_SHADER_STAGE_VERTEX_BIT,
+            .module = vertexShaderModule,
+            .pName = "main"};
+
+        const VkPipelineShaderStageCreateInfo fragShaderStageInfo{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+            .stage = VK_SHADER_STAGE_FRAGMENT_BIT,
+            .module = fragmentShaderModule,
+            .pName = "main"};
+
+        const VkPipelineShaderStageCreateInfo shaderStages[] = {
+            vertShaderStageInfo,
+            fragShaderStageInfo,
+        };
+
+        const VkPipelineVertexInputStateCreateInfo vertexInputInfo{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+        };
+
+        const VkPipelineInputAssemblyStateCreateInfo inputAssembly{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+            .topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
+            .primitiveRestartEnable = VK_FALSE};
+
+        const VkViewport viewport{.x = 0.0f,
+                                  .y = 0.0f,
+                                  .width = 1.f,
+                                  .height = 1.f,
+                                  .minDepth = 0.0f,
+                                  .maxDepth = 1.0f};
+
+        const VkRect2D scissor{.extent = {.width = 1, .height = 1}};
+
+        const VkPipelineViewportStateCreateInfo viewportState{.sType =
+                                                                  VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
+                                                              .viewportCount = 1,
+                                                              .pViewports = &viewport,
+                                                              .scissorCount = 1,
+                                                              .pScissors = &scissor};
+
+        const VkPipelineRasterizationStateCreateInfo rasterizer{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+            .polygonMode = VK_POLYGON_MODE_FILL,
+            .cullMode = VK_CULL_MODE_NONE,
+            .frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE,
+            .lineWidth = 1.f};
+
+        const VkPipelineMultisampleStateCreateInfo multisampling{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+            .rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
+            .minSampleShading = 1.f};
+
+        const VkPipelineColorBlendAttachmentState colorBlendAttachment{
+            .blendEnable = VK_TRUE,
+            .srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
+            .dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
+            .colorBlendOp = VK_BLEND_OP_ADD,
+            .srcAlphaBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
+            .dstAlphaBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
+            .alphaBlendOp = VK_BLEND_OP_ADD,
+            .colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT |
+                              VK_COLOR_COMPONENT_A_BIT};
+
+        const VkPipelineColorBlendStateCreateInfo colorBlending{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+            .logicOpEnable = VK_FALSE,
+            .logicOp = VK_LOGIC_OP_COPY,
+            .attachmentCount = 1,
+            .pAttachments = &colorBlendAttachment,
+            .blendConstants = {0.f}};
+
+        constexpr auto swapchainFormat{VK_FORMAT_B8G8R8A8_UNORM};
+
+        const VkPipelineRenderingCreateInfo pipelineRenderingCreateInfo{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO,
+            .colorAttachmentCount = 1,
+            .pColorAttachmentFormats = &swapchainFormat,
+        };
+
+        const VkGraphicsPipelineCreateInfo pipelineInfo{
+            .sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
+            .pNext = &pipelineRenderingCreateInfo,
+            .stageCount = size(shaderStages),
+            .pStages = shaderStages,
+            .pVertexInputState = &vertexInputInfo,
+            .pInputAssemblyState = &inputAssembly,
+            .pViewportState = &viewportState,
+            .pRasterizationState = &rasterizer,
+            .pMultisampleState = &multisampling,
+            .pDepthStencilState = nullptr,
+            .pColorBlendState = &colorBlending,
+            .pDynamicState = nullptr,
+            .layout = pipelineLayout,
+            .renderPass = nullptr,
+            .subpass = 0,
+            .basePipelineHandle = VK_NULL_HANDLE,
+            .basePipelineIndex = -1,
+        };
+
+        return vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &m_dummyPipeline) ==
+               VK_SUCCESS;
+    }
+
+    void imgui::destroy_dummy_pipeline(VkDevice device)
+    {
+        if (m_dummyPipeline)
+        {
+            vkDestroyPipeline(device, m_dummyPipeline, nullptr);
+            m_dummyPipeline = nullptr;
+        }
     }
 }
