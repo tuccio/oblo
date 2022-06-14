a8a3f5ff311b8faa81b9af7c133653e2cffb6e92
Davide Tuccilli <d.tuccilli@gmail.com>
2022-06-15 00:59:50 +0200

    Hacky solution for transparency issue with imgui without render passes

--- sandbox/vulkan/src/sandbox/sandbox_app.cpp
+++ sandbox/vulkan/src/sandbox/sandbox_app.cpp
@@ -243,9 +243,8 @@ namespace oblo::vk
                 .pColorAttachments = &colorAttachmentInfo,
             };
 
-            vkCmdBeginRendering(commandBuffer, &renderInfo);
-
             vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_graphicsPipeline);
+            vkCmdBeginRendering(commandBuffer, &renderInfo);
 
             const VkBuffer vertexBuffers[] = {m_positions.buffer, m_colors.buffer};
             constexpr VkDeviceSize offsets[] = {0, 0};
@@ -548,12 +547,6 @@ namespace oblo::vk
 
         const VkPipelineColorBlendAttachmentState colorBlendAttachment{
             .blendEnable = VK_FALSE,
-            .srcColorBlendFactor = VK_BLEND_FACTOR_ONE,
-            .dstColorBlendFactor = VK_BLEND_FACTOR_ZERO,
-            .colorBlendOp = VK_BLEND_OP_ADD,
-            .srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE,
-            .dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
-            .alphaBlendOp = VK_BLEND_OP_ADD,
             .colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT |
                               VK_COLOR_COMPONENT_A_BIT};
 
