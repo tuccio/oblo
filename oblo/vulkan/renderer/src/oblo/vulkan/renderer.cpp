61162b74734949809f1d6de6a0229d52fd2c14b2
Davide Tuccilli <d.tuccilli@gmail.com>
2023-07-15 18:03:26 +0200

    Generalize a little to run multiple render graphs

--- oblo/vulkan/renderer/src/oblo/vulkan/renderer.cpp
+++ oblo/vulkan/renderer/src/oblo/vulkan/renderer.cpp
@@ -1,15 +1,25 @@
 #include <oblo/vulkan/renderer.hpp>
 
+#include <oblo/render_graph/render_graph.hpp>
 #include <oblo/render_graph/render_graph_builder.hpp>
+#include <oblo/render_graph/render_graph_seq_executor.hpp>
 #include <oblo/vulkan/error.hpp>
-#include <oblo/vulkan/nodes/blit_image_node.hpp>
-#include <oblo/vulkan/nodes/deferred.hpp>
-#include <oblo/vulkan/nodes/forward.hpp>
 #include <oblo/vulkan/renderer_context.hpp>
+#include <oblo/vulkan/resource_manager.hpp>
+#include <oblo/vulkan/single_queue_engine.hpp>
 #include <oblo/vulkan/texture.hpp>
 
 namespace oblo::vk
 {
+    struct renderer::render_graph_data
+    {
+        render_graph graph;
+        render_graph_seq_executor executor;
+    };
+
+    renderer::renderer() = default;
+    renderer::~renderer() = default;
+
     bool renderer::init(const renderer::initializer& context)
     {
         m_allocator = &context.allocator;
@@ -27,33 +37,7 @@ namespace oblo::vk
         m_stringInterner.init(64);
         m_renderPassManager.init(m_engine->get_device(), m_stringInterner, m_dummy);
 
-#if 0
-        const auto ec = render_graph_builder<renderer_context>{}
-                            .add_node<deferred_gbuffer_node>()
-                            .add_node<deferred_lighting_node>()
-                            .add_node<blit_image_node>()
-                            // .add_edge(&deferred_gbuffer_node::gbuffer, &deferred_lighting_node::gbuffer)
-                            .add_input<allocated_buffer>("camera")
-                            .add_input<h32<texture>>("final_render_target")
-                            .connect(&deferred_gbuffer_node::test, &deferred_lighting_node::test)
-                            .connect(&deferred_gbuffer_node::test, &blit_image_node::source)
-                            .connect_input<h32<texture>>("final_render_target", &blit_image_node::destination)
-                            .build(m_graph, m_executor);
-#else
-        const auto ec = render_graph_builder<renderer_context>{}
-                            .add_node<forward_node>()
-                            .add_input<h32<texture>>("final_render_target")
-                            .connect_input<h32<texture>>("final_render_target", &forward_node::renderTarget)
-                            .build(m_graph, m_executor);
-#endif
-
-        if (ec)
-        {
-            return false;
-        }
-
-        renderer_context rendererContext{.renderer = *this, .frameAllocator = context.frameAllocator};
-        return m_executor.initialize(&rendererContext);
+        return true;
     }
 
     void renderer::shutdown(frame_allocator& frameAllocator)
@@ -61,7 +45,13 @@ namespace oblo::vk
         m_meshes.shutdown(*m_allocator, *m_resourceManager);
 
         renderer_context rendererContext{.renderer = *this, .frameAllocator = frameAllocator};
-        m_executor.shutdown(&rendererContext);
+
+        for (auto& [graph, executor] : m_renderGraphs.values())
+        {
+            executor.shutdown(&rendererContext);
+        }
+
+        m_renderGraphs.clear();
 
         m_renderPassManager.shutdown();
         m_resourceManager->destroy(*m_allocator, m_dummy);
@@ -69,20 +59,61 @@ namespace oblo::vk
 
     void renderer::update(const update_context& context)
     {
-        // Set-up the graph inputs
-        auto* const finalRenderTarget = m_graph.find_input<h32<texture>>("final_render_target");
-        OBLO_ASSERT(finalRenderTarget);
-
-        *finalRenderTarget = context.swapchainTexture;
-
         renderer_context rendererContext{
             .renderer = *this,
             .frameAllocator = context.frameAllocator,
             .commandBuffer = &context.commandBuffer,
         };
 
-        m_executor.execute(&rendererContext);
+        for (auto& [graph, executor] : m_renderGraphs.values())
+        {
+            executor.execute(&rendererContext);
+        }
 
         m_stagingBuffer.flush();
+    }
+
+    h32<render_graph> renderer::create_graph(const render_graph_builder<renderer_context>& builder,
+                                             frame_allocator& frameAllocator)
+    {
+        render_graph graph;
+        render_graph_seq_executor executor;
+
+        if (builder.build(graph, executor))
+        {
+            return {};
+        }
+
+        renderer_context rendererContext{
+            .renderer = *this,
+            .frameAllocator = frameAllocator,
+        };
+
+        if (!executor.initialize(&rendererContext))
+        {
+            return {};
+        }
+
+        h32<render_graph> newHandle{++m_lastRenderGraphId};
+        m_renderGraphs.emplace(newHandle, std::move(graph), std::move(executor));
+        return newHandle;
+    }
+
+    void renderer::destroy_graph(h32<render_graph> handle, frame_allocator& frameAllocator)
+    {
+        auto* const graph = m_renderGraphs.try_find(handle);
+
+        if (graph)
+        {
+            renderer_context rendererContext{.renderer = *this, .frameAllocator = frameAllocator};
+            graph->executor.shutdown(&rendererContext);
+            m_renderGraphs.erase(handle);
+        }
+    }
+
+    render_graph* renderer::find_graph(h32<render_graph> handle)
+    {
+        auto* const graph = m_renderGraphs.try_find(handle);
+        return graph ? &graph->graph : nullptr;
     }
 }
