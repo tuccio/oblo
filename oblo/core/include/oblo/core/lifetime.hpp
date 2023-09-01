cb849e003ba02631335550d74389ffbac8aa55b4
Davide Tuccilli <d.tuccilli@gmail.com>
2023-09-02 01:19:38 +0200

    Lifetime

---
+++ oblo/core/include/oblo/core/lifetime.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <cstddef>
+#include <type_traits>
+
+namespace oblo
+{
+    template <typename T>
+    T* start_lifetime_as(void* ptr) noexcept
+    {
+        auto* const bytes = new (ptr) std::byte[sizeof(T)];
+        auto* const object = reinterpret_cast<T*>(bytes);
+        // Make sure to dereference to start the lifetime
+        (void) *object;
+        return object;
+    }
+
+    template <typename T>
+    const T* start_lifetime_as(const void* ptr) noexcept
+    {
+        return start_lifetime_as(const_cast<void*>(ptr));
+    }
+
+    template <typename T>
+    T* start_lifetime_as_array(void* ptr, std::size_t n) noexcept
+    {
+        auto* const bytes = new (ptr) std::byte[sizeof(T) * n];
+        auto* const object = reinterpret_cast<T*>(bytes);
+        // Make sure to dereference to start the lifetime
+        (void) *object;
+        return object;
+    }
+
+    template <typename T>
+    const T* start_lifetime_as_array(const void* ptr, std::size_t n) noexcept
+    {
+        return start_lifetime_as_array(const_cast<void*>(ptr), n);
+    }
+}
