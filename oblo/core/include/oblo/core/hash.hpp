6a4cfee93ad044a00b07da4c68aa87d9e79bc918
Davide Tuccilli <d.tuccilli@gmail.com>
2023-09-02 01:19:02 +0200

    Fix hash build errors

--- oblo/core/include/oblo/core/hash.hpp
+++ oblo/core/include/oblo/core/hash.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
-#include <core/types.hpp>
+#include <oblo/core/types.hpp>
 
 namespace oblo
 {
-    constexpr void hash_mix(u32 seed, u32 hash)
+    constexpr u32 hash_mix(u32 seed, u32 hash)
     {
-        seed ^= hash + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+        seed ^= hash + 0x9e3779b9u + (seed << 6) + (seed >> 2);
         return seed;
     }
 
     constexpr u64 hash_mix(u64 seed, u64 hash)
     {
-        seed ^= hash + 0x9e3779b97f4a7c15 + (seed << 6) + (seed >> 2);
+        seed ^= hash + 0x9e3779b97f4a7c15ull + (seed << 6) + (seed >> 2);
         return seed;
     }
 }
