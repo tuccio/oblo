b8250bfdc4d184205ce3fcd0671e8c53793339a8
Davide <d.tuccilli@gmail.com>
2023-11-06 22:30:12 +0100

    Simplify angle class and reflect it

--- oblo/graphics/include/oblo/graphics/components/camera_component.hpp
+++ oblo/graphics/include/oblo/graphics/components/camera_component.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <oblo/core/types.hpp>
+#include <oblo/math/angle.hpp>
 #include <oblo/math/vec3.hpp>
 
 namespace oblo
 {
     struct camera_component
     {
-        f32 fovy;
+        radians fovy;
         f32 near;
         f32 far;
     };
