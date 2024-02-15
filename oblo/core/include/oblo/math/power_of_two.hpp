7265d3247bfdd1e472cfd0234535942010b982d9
Davide <d.tuccilli@gmail.com>
2024-02-16 00:53:28 +0100

    Some more power of two functions

--- oblo/core/include/oblo/math/power_of_two.hpp
+++ oblo/core/include/oblo/math/power_of_two.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <bit>
+#include <concepts>
+
+#include <oblo/core/types.hpp>
+
 namespace oblo
 {
-    template <typename T>
+    template <std::unsigned_integral T>
     constexpr bool is_power_of_two(T x)
     {
         return (x != 0) && ((x & (x - 1)) == 0);
@@ -11,7 +16,7 @@ namespace oblo
 #pragma warning(push)
 #pragma warning(disable : 4146)
 
-    template <typename T>
+    template <std::unsigned_integral T>
     constexpr T align_power_of_two(T unaligned, T alignment)
     {
         const auto mask = alignment - 1;
@@ -19,4 +24,11 @@ namespace oblo
     }
 
 #pragma warning(pop)
+
+    template <std::unsigned_integral T>
+    constexpr T round_up_power_of_two(T x)
+    {
+        const auto log2 = std::countl_zero(x - 1);
+        return T(1) << (8 * sizeof(T) - log2);
+    }
 }
