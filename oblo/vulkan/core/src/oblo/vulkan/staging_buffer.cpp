b0b4b5815fb09b55efdfb490426c73e31e4f0b9b
Davide <d.tuccilli@gmail.com>
2024-03-02 16:19:41 +0100

    Made expected_monostate clearer

--- oblo/vulkan/core/src/oblo/vulkan/staging_buffer.cpp
+++ oblo/vulkan/core/src/oblo/vulkan/staging_buffer.cpp
@@ -351,7 +351,7 @@ namespace oblo::vk
 
         if (available < srcSize)
         {
-            return expected_monostate::failure;
+            return expected_monostate{};
         }
 
         const auto segmentedSpan = m_impl.ring.fetch(srcSize);
