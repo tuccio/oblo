ab1cb158bf8aadb1f1a0485269230fd51c51d63f
Davide <d.tuccilli@gmail.com>
2024-03-03 18:35:41 +0100

    Get tests running

--- oblo/vulkan/renderer/resources/shaders/renderer/constants.glsl
+++ oblo/vulkan/renderer/resources/shaders/renderer/constants.glsl
@@ -1,14 +1,14 @@
 #ifndef OBLO_INCLUDE_RENDERER_CONSTANTS
 #define OBLO_INCLUDE_RENDERER_CONSTANTS
 
-float make_positive_infinity()
+float float_positive_infinity()
 {
-    return uintBitsToFloat(0x7F800000);;
+    return uintBitsToFloat(0x7F800000);
 }
 
-float make_negative_infinity()
+float float_negative_infinity()
 {
-    return uintBitsToFloat(0xFF800000);;
+    return uintBitsToFloat(0xFF800000);
 }
 
 #endif
