db459fb63f457b3f5a1f5da7e0f704b74646612b
Davide <d.tuccilli@gmail.com>
2024-03-15 20:53:07 +0100

    Customizable panic handler

--- oblo/vulkan/core/src/oblo/vulkan/error.cpp
+++ oblo/vulkan/core/src/oblo/vulkan/error.cpp
@@ -5,9 +5,30 @@
 
 namespace oblo::vk
 {
+    namespace
+    {
+        void panic_default_handler(const char* file, int line, const char* call, VkResult result, void*)
+        {
+            std::fprintf(stderr, "[Vulkan Error] %s:%d %s (%d)", file, line, call, result);
+        }
+
+        void* g_userdata{};
+        panic_handler g_handler{panic_default_handler};
+    }
+
     void panic(const char* file, int line, const char* call, VkResult result)
     {
-        std::fprintf(stderr, "[Vulkan Error] %s:%d %s (%d)", file, line, call, result);
+        if (g_handler)
+        {
+            g_handler(file, line, call, result, g_userdata);
+        }
+
         std::terminate();
+    }
+
+    void set_panic_handler(panic_handler handler, void* userdata)
+    {
+        g_handler = handler ? handler : panic_default_handler;
+        g_userdata = userdata;
     }
 }
