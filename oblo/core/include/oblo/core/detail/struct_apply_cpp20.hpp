e3bf76805c3d6b5e799ed93772bd46cee6f1dbcd
Davide <d.tuccilli@gmail.com>
2024-04-21 10:55:08 +0200

    Fix struct_apply not returning in all implementations

--- oblo/core/include/oblo/core/detail/struct_apply_cpp20.hpp
+++ oblo/core/include/oblo/core/detail/struct_apply_cpp20.hpp
@@ -6,7 +6,7 @@ namespace oblo
 {
     /* clang-format off */
     template <typename T, typename Function, typename... Ts>
-    auto struct_apply(Function&& f, T&& obj)
+    constexpr auto struct_apply(Function&& f, T&& obj)
     {
         if constexpr (requires { [&obj] { auto&& [m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19, m20, m21, m22, m23, m24, m25, m26, m27, m28, m29, m30, m31, m32, m33, m34, m35, m36, m37, m38, m39, m40, m41, m42, m43, m44, m45, m46, m47, m48, m49, m50, m51, m52, m53, m54, m55, m56, m57, m58, m59, m60, m61, m62, m63] = obj; }; })
         {
