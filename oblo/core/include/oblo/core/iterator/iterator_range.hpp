20535ff06cecaa6e2bf9d0be4acb832ad19217e4
Davide <d.tuccilli@gmail.com>
2024-06-09 00:55:07 +0200

    Reverse range

---
+++ oblo/core/include/oblo/core/iterator/iterator_range.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace oblo
+{
+    template <typename Iterator>
+    class iterator_range
+    {
+    public:
+        iterator_range() = default;
+        iterator_range(const iterator_range&) = default;
+        iterator_range(iterator_range&&) noexcept = default;
+
+        iterator_range(const Iterator& begin, const Iterator& end) : m_begin{begin}, m_end{end} {}
+
+        iterator_range& operator=(const iterator_range&) = default;
+        iterator_range& operator=(iterator_range&&) noexcept = default;
+
+        auto begin() const
+        {
+            return m_begin;
+        }
+
+        auto end() const
+        {
+            return m_end;
+        }
+
+    private:
+        Iterator m_begin;
+        Iterator m_end;
+    };
+
+    template <typename Iterator>
+    iterator_range(Iterator, Iterator) -> iterator_range<Iterator>;
+}
