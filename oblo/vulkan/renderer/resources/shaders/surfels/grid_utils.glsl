9cf8f9a112c5dfa875b1fe5b464716156cc8676e
Davide <d.tuccilli@gmail.com>
2025-01-19 18:12:26 +0100

    Surfels being inserted in too many grid cells

--- oblo/vulkan/renderer/resources/shaders/surfels/grid_utils.glsl
+++ oblo/vulkan/renderer/resources/shaders/surfels/grid_utils.glsl
@@ -8,12 +8,6 @@ void surfel_commit_to_grid(in uint surfelId, in uint cellIndex);
 void surfel_scatter_to_grid(
     in surfel_grid_header gridHeader, in ivec3 cell, in uint surfelId, in vec3 positionWS, in float radius)
 {
-    if (surfel_grid_has_cell(gridHeader, cell))
-    {
-        const uint cellIndex = surfel_grid_cell_index(gridHeader, cell);
-        surfel_commit_to_grid(surfelId, cellIndex);
-    }
-
     // Search neighboring cells and accumulate overlapping surfels
     [[unroll]] for (int x = -1; x <= 1; ++x)
     {
@@ -21,18 +15,16 @@ void surfel_scatter_to_grid(
         {
             [[unroll]] for (int z = -1; z <= 1; ++z)
             {
-                if (x == 0 && y == 0 && z == 0)
+                const ivec3 neighboringCell = cell + ivec3(x, y, z);
+
+                if (!surfel_grid_has_cell(gridHeader, neighboringCell))
                 {
-                    // Skip the cell we already dealt with
                     continue;
                 }
 
-                const vec3 delta = vec3(radius) * vec3(x, y, z);
-                const vec3 posOnSurface = positionWS + delta;
+                const vec3 posOnSurface = positionWS + radius * vec3(x, y, z);
 
-                const ivec3 neighboringCell = surfel_grid_find_cell(gridHeader, posOnSurface);
-
-                if (neighboringCell != cell)
+                if (neighboringCell == surfel_grid_find_cell(gridHeader, posOnSurface))
                 {
                     const uint neighboringCellIndex = surfel_grid_cell_index(gridHeader, neighboringCell);
                     surfel_commit_to_grid(surfelId, neighboringCellIndex);
