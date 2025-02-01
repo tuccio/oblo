b6f802f2c32ba50cbfd8b607a585eb959e013c00
Davide <d.tuccilli@gmail.com>
2025-02-01 15:55:05 +0100

    Update style

--- oblo/editor/common/src/oblo/editor/ui/style.cpp
+++ oblo/editor/common/src/oblo/editor/ui/style.cpp
@@ -78,6 +78,7 @@ namespace oblo::editor
             colors[ImGuiCol_Tab] = ImVec4(0.05f, 0.05f, 0.07f, 1.00f);
             colors[ImGuiCol_TabHovered] = ImVec4(0.12f, 0.13f, 0.15f, 1.00f);
             colors[ImGuiCol_TabActive] = ImVec4(0.10f, 0.11f, 0.12f, 1.00f);
+            colors[ImGuiCol_TabSelectedOverline] = ImVec4(0.90f, 0.67f, 0.95f, 1.00f);
             colors[ImGuiCol_TabUnfocused] = ImVec4(0.05f, 0.05f, 0.07f, 1.00f);
             colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.08f, 0.09f, 0.10f, 1.00f);
             colors[ImGuiCol_DockingPreview] = ImVec4(0.57f, 0.35f, 0.62f, 1.00f);
