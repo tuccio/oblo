d1ddf1e4032e3fc3b73ecf7f143b201cb6ba834e
Davide <d.tuccilli@gmail.com>
2025-04-04 23:11:24 +0200

    Add annotation to vk renderer

--- oblo/vulkan/renderer/include/oblo/vulkan/data/components.hpp
+++ oblo/vulkan/renderer/include/oblo/vulkan/data/components.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <oblo/core/reflection/annotations.hpp>
 #include <oblo/vulkan/data/handles.hpp>
 
 namespace oblo::vk
@@ -9,21 +10,21 @@ namespace oblo::vk
     struct draw_instance_component
     {
         mesh_handle mesh;
-    };
+    } OBLO_COMPONENT(GpuComponent = "i_MeshHandles");
 
     struct draw_instance_id_component
     {
         // We use 24 bits, because that is what the ray tracing pipeline allows for custom ids
         // We reserve 4 for the instance table and 20 for the instance index
         u32 rtInstanceId;
-    };
+    } OBLO_COMPONENT();
 
     struct draw_mesh_component
     {
         h32<draw_mesh> mesh;
-    };
+    } OBLO_COMPONENT();
 
     struct draw_raytraced_tag
     {
-    };
+    } OBLO_TAG();
 }
--- oblo/vulkan/renderer/src/oblo/vulkan/data/tags_internal.hpp
+++ oblo/vulkan/renderer/src/oblo/vulkan/data/tags_internal.hpp
@@ -1,18 +1,22 @@
+#pragma once
+
+#include <oblo/core/reflection/annotations.hpp>
+
 namespace oblo::vk
 {
     struct mesh_index_none_tag
     {
-    };
+    } OBLO_TAG();
 
     struct mesh_index_u8_tag
     {
-    };
+    } OBLO_TAG();
 
     struct mesh_index_u16_tag
     {
-    };
+    } OBLO_TAG();
 
     struct mesh_index_u32_tag
     {
-    };
+    } OBLO_TAG();
 }
