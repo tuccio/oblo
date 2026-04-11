4ede714322849429e0058369357a234da643f979
Davide <d.tuccilli@gmail.com>
2026-04-11 08:45:57 +0200

    Make position and barycentric coords function more reusable

--- oblo/renderer/resources/shaders/visibility/visibility_utils.glsl
+++ oblo/renderer/resources/shaders/visibility/visibility_utils.glsl
@@ -42,4 +42,53 @@ vec3 visibility_calculate_position_ndc_3d(in mat4 viewProjection, in vec3 prevPo
     return prevPositionCS.xyz / prevPositionCS.w;
 }
 
+bool visibility_calculate_position_and_barycentric_coords(in uvec2 screenPos,
+    in uvec2 resolution,
+    in camera_buffer camera,
+    in triangle triangleWS,
+    out vec3 positionWS,
+    out barycentric_coords bc,
+    out barycentric_coords bcDDX,
+    out barycentric_coords bcDDY)
+{
+    // Cast a ray from the camera to the near plane and calculate the distance of the ray hit to the plane on the
+    // triangle in world space, we use that to derive the position in world space
+    const vec2 ndc = screen_to_ndc(screenPos, resolution);
+    const ray cameraRay = visibility_calculate_camera_ray(camera, ndc);
+
+    float intersectionDistance;
+
+    // Really the plan should be hitting here, since we already know the triangle was rendered by the rasterizer, we
+    // mostly want to calculate at what distance it does
+    if (!distance_from_triangle_plane(cameraRay, triangleWS, intersectionDistance))
+    {
+        return false;
+    }
+
+    ray cameraRayDDX;
+    cameraRayDDX.origin = camera.position;
+    cameraRayDDX.direction = subgroupQuadSwapHorizontal(cameraRay.direction);
+
+    float intersectionDistanceDDX;
+    distance_from_triangle_plane(cameraRayDDX, triangleWS, intersectionDistanceDDX);
+
+    ray cameraRayDDY;
+    cameraRayDDY.origin = camera.position;
+    cameraRayDDY.direction = subgroupQuadSwapVertical(cameraRay.direction);
+
+    // We do the same ray tracing with the nearby quads, so we can calculate UV gradients for our sampler
+    float intersectionDistanceDDY;
+    distance_from_triangle_plane(cameraRayDDX, triangleWS, intersectionDistanceDDY);
+
+    positionWS = ray_point_at(cameraRay, intersectionDistance);
+    const vec3 positionDDX = ray_point_at(cameraRayDDX, intersectionDistanceDDX);
+    const vec3 positionDDY = ray_point_at(cameraRayDDY, intersectionDistanceDDY);
+
+    barycentric_calculate(bc, triangleWS.v, positionWS);
+    barycentric_calculate(bcDDX, triangleWS.v, positionDDX);
+    barycentric_calculate(bcDDX, triangleWS.v, positionDDY);
+
+    return true;
+}
+
 #endif
